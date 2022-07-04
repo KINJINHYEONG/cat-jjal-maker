12ee46690b7ed95cb57016d81daff5ba66b42488
kimjinhyeong <kjh86@lguplus.co.kr>
2022-07-04 16:25:19 +0900

    made CRA

--- cat-jjal-maker-cra/src/App.js
+++ cat-jjal-maker-cra/src/App.js
@@ -57,7 +57,7 @@ const Form = ({ updateMainCat }) => {
         value={value}
         onChange={handleInputChange}
       />
-      <button type="submit">생성2</button>
+      <button type="submit">생성3</button>
       <p style={{ color: "red" }}>{errorMessage}</p>
     </form>
   );
